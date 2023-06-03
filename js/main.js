ede3ff55872ce2036b5d7c27add2059739ab80a5
Sergio Olivo <sergio_oliver009@hotmail.com>
2023-06-03 02:37:50 -0500

    portafolio

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 
 //Mostrar menu hamburguesa
 
-function handleMain(){
+function handleMain() {
     const bxMenuHTML = document.querySelector('.bx-menu');
-    const menuHTML = document.querySelector('.menu')    
-    bxMenuHTML.addEventListener('click', () =>{
+    const menuHTML = document.querySelector('.menu')
+    bxMenuHTML.addEventListener('click', () => {
         menuHTML.classList.add('menuShow');
         console.log('click');
     })
@@ -13,14 +13,34 @@ handleMain();
 
 //Ocultar menu hamburguesa en clik a enlace
 
-function handdleMainHidden(){
+function handdleMainHidden() {
     const linkHTML = document.querySelectorAll('.link')
-    const menuHTML = document.querySelector('.menu');    
-    linkHTML.forEach(link =>{
+    const menuHTML = document.querySelector('.menu');
+    linkHTML.forEach(link => {
         link.addEventListener('click', () => {
             menuHTML.classList.remove('menuShow')
-        })    
+        })
     })
 }
 handdleMainHidden()
 
+//Ocultar menu hamburguesa en clik a enlace
+
+function showShadowNav() {
+    const navHTML = document.querySelector('.nav');
+    let prevScrollPos = window.pageYOffset;
+
+    window.addEventListener('scroll', function () {
+        const currentScrollPos = window.pageYOffset;
+
+        if (currentScrollPos < prevScrollPos) {
+            navHTML.classList.remove('nav_shadow');
+        } else {
+            navHTML.classList.add('nav_shadow');
+        }
+
+        prevScrollPos = currentScrollPos;
+    });
+}
+
+showShadowNav();
